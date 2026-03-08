542e2e31342b51ef8f0996a0feaed13a3c4d941e
Aixnpy <chesschessxx1@gmail.com>
2026-03-09 03:19:41 +0530

    Update __init__.py

--- automatax1/__init__.py
+++ automatax1/__init__.py
@@ -1,8 +1,1 @@
-"""Core automation modules for automatax1"""
-from .device import Device
-from .touch import Touch
-from .keyboard import Keyboard
-from .selector import Selector
-from .service import Service
-
-__all__ = ['Device', 'Touch', 'Keyboard', 'Selector', 'Service']
+not done
