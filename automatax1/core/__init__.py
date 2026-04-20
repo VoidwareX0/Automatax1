c5b81e066d01b42b5bf0f76a3f9d6959042ca5a3
Aixnpy <chesschessxx1@gmail.com>
2026-04-20 15:51:47 +0530

    Delete __init__.py

--- automatax1/core/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Core automation modules for automatax1"""
-
-from .device import Device, list_devices
-from .touch import Touch
-from .keyboard import Keyboard
-from .selector import Selector, Element
-from .service import Service
-
-__all__ = ['Device', 'Touch', 'Keyboard', 'Selector', 'Element', 'Service', 'list_devices']
